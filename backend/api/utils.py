6be43308209a85719122e9dfdbf72fb7137cb270
Николай Королёв <korolevnikolay96@gmail.com>
2023-08-27 18:00:02 +0300

    fix tests pep8_v4

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -11,8 +11,7 @@ from borb.pdf import (Document,
 from borb.pdf.canvas.font.simple_font.true_type_font import TrueTypeFont
 from borb.pdf.canvas.color.color import HexColor
 from borb.pdf.canvas.layout.image.barcode import Barcode, BarcodeType
-
-from borb.pdf.canvas.layout.table import flexible_column_width_table
+from borb.pdf.canvas.layout.table import flexible_column_width_table as ft
 
 
 def create_pdf(text):
@@ -29,6 +28,16 @@ def create_pdf(text):
                       width=Decimal(64),
                       height=Decimal(64),
                       type=BarcodeType.QR,)
+    about = Paragraph("""
+                      Николай Королёв
+                      Github аккаунт""",
+                      font=custom_font,
+                      padding_top=Decimal(12),
+                      respect_newlines_in_text=True,
+                      font_color=HexColor("#666666"),
+                      font_size=Decimal(10),
+                      horizontal_alignment=Alignment.LEFT
+                      )
 
     layout.add(Paragraph('Список покупок',
                          font=custom_font,
@@ -45,20 +54,9 @@ def create_pdf(text):
                              font_size=Decimal(14)))
 
     layout.add(
-        flexible_column_width_table.FlexibleColumnWidthTable(
-                                    number_of_columns=2,
-                                    number_of_rows=1)
+        ft.FlexibleColumnWidthTable(number_of_columns=2, number_of_rows=1)
         .add(qr_code)
-        .add(Paragraph("""
-                       Николай Королёв
-                       Github аккаунт
-                       """,
-                       font=custom_font,
-                       padding_top=Decimal(12),
-                       respect_newlines_in_text=True,
-                       font_color=HexColor("#666666"),
-                       font_size=Decimal(10),
-                       horizontal_alignment=Alignment.LEFT)).no_borders())
+        .add(Paragraph(about)).no_borders())
 
     with open(Path("file.pdf"), "wb") as pdf_file_handle:
         PDF.dumps(pdf_file_handle, pdf)
