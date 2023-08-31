94980edd5defdb98c4be7b673401e4573d4801b6
Николай Королёв <korolevnikolay96@gmail.com>
2023-08-31 21:16:36 +0300

    fix pep8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # }
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
-       'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-       'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram_password'),
-       'HOST': os.getenv('DB_HOST', 'db'),
-       'PORT': os.getenv('DB_PORT', 5432)
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram_password'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
+        }
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
